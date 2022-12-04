08fde27d4dcb83e936e94c9dc74e4ec6039fc444
Antonio Cunha <antonio.frango@gmail.com>
2022-12-04 14:36:51 -0300

    Exercicios para fixacao bloco 4.2 - Procurando o indice de um array

--- scripts.js
+++ scripts.js
@@ -16,10 +16,25 @@ let searchForLastTask = tasksList[tasksList.length - 1];
 console.log(searchForLastTask);
 // Brincar com o cachorro */ 
 
-let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+/* let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
 
 tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
 tasksList.unshift('Andar')
 console.log(tasksList);
 
-// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe'] */
+
+/*let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.pop();  // remove a última tarefa
+tasksList.shift(); // remove o primeiro da lista
+console.log(tasksList);
+
+// [ 'Tomar café', 'Reunião' ] */ 
+
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+let indexOfTask = tasksList.indexOf('Tomar café');
+console.log(indexOfTask);
+
+// 1
