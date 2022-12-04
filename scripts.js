09373052d36654ebe7261230623b2a6274c19c41
Antonio Cunha <antonio.frango@gmail.com>
2022-12-04 14:28:56 -0300

    Exercicios para fixacao bloco 4.2 - Acessando Array pelo indice

--- scripts.js
+++ scripts.js
@@ -1,6 +1,17 @@
-let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+/*let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
 tasksList.push ('Passear');
 tasksList.push ('Comer');
 
-console.log(tasksList.length);
+console.log(tasksList.length); */
 // 3
+
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+tasksList.push ('Andar')
+
+let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask);
+// Tomar café
+
+let searchForLastTask = tasksList[tasksList.length - 1];
+console.log(searchForLastTask);
+// Brincar com o cachorro
