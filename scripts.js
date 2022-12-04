bbaf8eee5c7d407936ccdf64d724bbd664b3ca25
Antonio Cunha <antonio.frango@gmail.com>
2022-12-04 14:32:01 -0300

    Exercicios para fixacao bloco 4.2 - Add itens a lista

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ tasksList.push ('Comer');
 console.log(tasksList.length); */
 // 3
 
-let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+/* let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
 tasksList.push ('Andar')
 
 let searchForFirstTask = tasksList[0];
@@ -14,4 +14,12 @@ console.log(searchForFirstTask);
 
 let searchForLastTask = tasksList[tasksList.length - 1];
 console.log(searchForLastTask);
-// Brincar com o cachorro
+// Brincar com o cachorro */ 
+
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+tasksList.unshift('Andar')
+console.log(tasksList);
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
