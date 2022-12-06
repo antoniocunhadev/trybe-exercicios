ec9d0cd495e2dcc657bd820e5ae8f70877c34732
Antonio Cunha <antonio.frango@gmail.com>
2022-12-06 17:04:02 -0300

    Exercicios bonus 4.2 - Bubble sort

--- scripts.js
+++ scripts.js
@@ -1,254 +1,35 @@
-/*let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
-tasksList.push ('Passear');
-tasksList.push ('Comer');
+let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-console.log(tasksList.length); */
-// 3
-
-/* let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
-tasksList.push ('Andar')
-
-let searchForFirstTask = tasksList[0];
-console.log(searchForFirstTask);
-// Tomar café
-
-let searchForLastTask = tasksList[tasksList.length - 1];
-console.log(searchForLastTask);
-// Brincar com o cachorro */ 
-
-/* let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
-
-tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
-tasksList.unshift('Andar')
-console.log(tasksList);
-
-// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe'] */
-
-/*let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
-
-tasksList.pop();  // remove a última tarefa
-tasksList.shift(); // remove o primeiro da lista
-console.log(tasksList);
-
-// [ 'Tomar café', 'Reunião' ] */ 
-
-/* let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
-
-let indexOfTask = tasksList.indexOf('Tomar café');
-console.log(indexOfTask);
-
-// 1 */
-
-/*let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu.indexOf ('Serviços');
-
-console.log(menuServices); */
-
-/*let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-menu.push ('Contato')
-let indexOfPortfolio = menu.indexOf ('Portfólio') ;
-
-console.log(indexOfPortfolio);
-console.log(menu); */
-
-/* let cars = ['Saab', 'Volvo', 'BMW'];
-
-for (let index = 0; index < cars.length; index += 1) {
-  console.log (cars[index]); 
-} */
-
-/*let groceryList = ['Arroz', 'Feijão', 'Alfece', 'Melancia'];
-
-for (let index = 0; index < groceryList.length; index += 1) {
-  console.log (groceryList[index]);
-} */
-
-/* let numeros = [1,2,3,4,5];
-for(let numero of numeros) {
-  console.log(numero);
-}
-
-// resultado: 
-//1
-//2
-//3
-//4
-//5 */
- 
-/*let frase = 'Hello world, tudo bem com voces';
-for (let word of frase) {
-  console.log(word);
-}
-// resultado:
-// "H"
-// "e"
-// "l"
-// "l"
-// "o" */ 
-
-/*let arrOfNumbers = [10, 20, 30];
-for (let sum of arrOfNumbers) {
-  sum += 1;
-  console.log(sum);
-  console.log(arrOfNumbers);
-// Resultado: [10, 20, 30]
-}
-// 11
-// 21
-// 31 */
-
-/*let counter = 0;
-
-while(counter !== 5) {
-  counter += 1;
-} */ 
-
-/*function dice () { 
-    return Math.ceil(Math.random() * 6);
-}
-let d1 = dice();
-let d2 = dice();
-while (d1 === d2) {
-    d2 = dice();
-} */
-
-// Jogo da MegaSema
-/*let meusNumerosDaMega = [3, 6, 10, 45, 47, 59];
-
-let number1 = Math.floor(Math.random() * 60) +1;
-let number2 = Math.floor(Math.random() * 60) +1;
-let number3 = Math.floor(Math.random() * 60) +1;
-let number4 = Math.floor(Math.random() * 60) +1;
-let number5 = Math.floor(Math.random() * 60) +1;
-let number6 = Math.floor(Math.random() * 60) +1;
-
-
-let resultadoMegaSena = [number1, number2, number3, number4, number5, number6];
-
-let acertos = 0;
-
-for (let index = 0; index < meusNumerosDaMega.length; index += 1) {
-  console.log ('Meus numeros da Mega: ', meusNumerosDaMega[index]);
-  for (let index2 = 0; index2 < resultadoMegaSena.length; index2 += 1) {
-    console.log ('Resultado da Mega Sena: ', resultadoMegaSena[index2]);
-    if (meusNumerosDaMega[index] === resultadoMegaSena[index2]) {
-      acertos = acertos + 1;
+for (let index = 1; index < array.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (array[index] < array[secondIndex]) {
+      let position = array[index];
+      array[index] = array[secondIndex];
+      array[secondIndex] = position;
     }
   }
 }
-console.log ('Numeros de acertos: ', acertos); */
-
-// Exercicio pratico
-
-let numbers = [5, 9, 3, 19, 70, 100, 8, 2, 35, 27];
-
-for (let index = 0; index < numbers.length; index += 1) {
-  console.log(numbers[index]);
-}
-
-let somaTotal = 0;
-
-for (let index = 0; index < numbers.length; index += 1){
-  somaTotal += numbers[index];
-}
-console.log(somaTotal)
-
-let media = 0
-
-for (let index = 0; index < numbers.length; index += 1){
-  media = somaTotal / 10;
-}
-console.log(media)
-if (media > 20) {
-  console.log ('Valor maior que 20')
-} else {
-  console.log ('Valor menor que 20')
-}
-
-
-let maiorNumero = Math.max.apply(Math,numbers)
-console.log (maiorNumero)
-
-// resolucao para encontrar o maior numero de uma array - Gabarito
-
-let higherNumber = numbers[0];
-
-for (let index = 0; index < numbers.length; index += 1) {
-  if (numbers[index] > higherNumber) {
-    higherNumber = numbers[index];
-  }
-}
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
-let impar = []
-
-for (let index = 0; index < numbers.length; index += 1){
-  numerosImpares = (numbers[index] % 2);
-  if (numerosImpares === 1) {
-    impar.push (numbers[index])
-  } else {
-    ('Nenhum ímpar valor foi encontrado')
-  }
-}  
-  console.log (impar.length, impar)
-
-  // resolucao para encontrar os numeros impares de uma array - Gabarito
-
-  let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-  let result = 0;
-  
-  for (let index = 0; index < numbers.length; index += 1) {
-    if (numbers[index] % 2 !== 0) {
-      result += 1;
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < numbers.length; secondIndex += 1) {
+    if (numbers[index] < numbers[secondIndex]){
+      let posicao = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = posicao;
     }
   }
-  
-  if (result === 0) {
-    console.log('nenhum valor ímpar encontrado');
-  } else {
-    console.log(result);
-  }
-  
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menorNumero = Math.min.apply(Math,numbers)
-console.log (menorNumero)
-
-// resolucao para encontrar o menor numero de um array - Gabarito
+}
+console.log (numbers)
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
-  let smallestNumber = numbers[0];
-  
-  for (let index = 1; index < numbers.length; index += 1) {
-    if (numbers[index] < smallestNumber) {
-      smallestNumber = numbers[index];
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < numbers.length; secondIndex += 1) {
+    if (numbers[secondIndex] < numbers[index]){
+      let posicao = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = posicao;
     }
   }
-  
-  console.log(smallestNumber);
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
-let array = []
-
-for (let index = 1; index <= 25; index += 1) {
-  array += " Indice " + index;
 }
-console.log (array)
-
-// resolucao para criar um array que vá de 01 a 25  - Gabarito
-
-let numbers = [];
-
-for (let index = 1; index <= 25; index += 1) {
-  numbers.push(index);
-}
-
-console.log(numbers);
-
-for (let index = 0; index < numbers.length; index += 1) {
-  console.log(numbers[index] / 2);
-};
-
-
+console.log (numbers)
