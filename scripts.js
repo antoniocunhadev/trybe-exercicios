2e2ddb52329fca348cedcb5c57ea22dca2eb012a
Antonio Cunha <antonio.frango@gmail.com>
2022-12-06 18:16:13 -0300

    Exercicios pratico 4.3 - Somando todos os valores de um array

--- scripts.js
+++ scripts.js
@@ -2,9 +2,8 @@ let fruits = [3, 4, 10, 1, 12];
 let somaFruits =0
 
 for (let index = 0; index < fruits.length; index += 1) {
-  somaFruits = fruits[index] += fruits
+  somaFruits += fruits[index]
 }
-console.log (fruits)
 
 if (somaFruits > 15) {
   console.log (somaFruits)
