1798c7029df7568fb8342432c66ef916e94fcbfe
Antonio Cunha <antonio.frango@gmail.com>
2022-12-08 12:41:11 -0300

    Exercicios pratico 4.3 - Descombrir a maior e a menor palavra do arra

--- scripts.js
+++ scripts.js
@@ -20,6 +20,26 @@ for (let index = word.length -1; index >=0; index -=1) {
 
 console.log (newWord)
 
+// Exercício prático - Imprimir a maior e a menor palavra do Array
 
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = array [0]
+
+for (let index = 0; index < array.length; index +=1) {
+  if (array[index].length > maiorPalavra.length) {
+    maiorPalavra = array[index]
+  }
+}
+console.log (maiorPalavra)
+
+let menorPalavra = array [0]
+
+for (let index = 0; index < array.length; index +=1) {
+  if (array[index].length < menorPalavra.length) {
+    menorPalavra = array[index]
+  }
+}
+
+console.log (menorPalavra)
 
 
