d2888ce8a390d6165ead10e9aad49cc6c7306544
Antonio Cunha <antonio.frango@gmail.com>
2022-12-08 11:04:53 -0300

    Exercicios pratico 4.3 - Fatorial de 10

--- scripts.js
+++ scripts.js
@@ -1,40 +1,11 @@
-let fruits = [3, 4, 10, 1, 12];
-let somaFruits =0
+// Exercicio prático - fatorial de 10
 
-for (let index = 0; index < fruits.length; index += 1) {
-  somaFruits += fruits[index]
+let fatorial = 10
+let resultado = fatorial
+
+for (let index = 1; index < fatorial; index +=1) {
+  resultado *= index
 }
+console.log ('O fatorial de ' + fatorial + ' é: ' + resultado)
 
-if (somaFruits > 15) {
-  console.log (somaFruits)
-} else {
-  console.log ('Valor menor que 16')
-}
 
-const n = 9 ;
-
-let resultado = 100;
-for (let index = 0; index <= n; index += 1) {
-  resultado -= index;
-};
-console.log(resultado);
-
-const n = 5;
-
-let resultado = 0;
-for (let index = 0; index <= n; index += 1) {
-  resultado =+ index
-};
-console.log(resultado)
-
-let array = []
-
-for (let index = 1; index <= 100; index += 1) {
-  array.push(index)
-}
-console.log (array)
-let soma = 0;
-for (let index = 0; index < array.length; index += 1){
-  soma += array[index];
-}
-console.log(soma)
