179a197b0bfd05b61e2730c81912ffce00d826e8
Antonio Cunha <antonio.frango@gmail.com>
2022-12-08 14:37:00 -0300

    Exercicios bônus 4.3 - Criar forma geometrica II

--- scripts.js
+++ scripts.js
@@ -54,3 +54,12 @@ for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
   console.log(inputLine);
 };
 
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+  console.log(inputLine); 
+};
+
