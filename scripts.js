a15507d2ff2a8254c7f81fa34341a8d457f880ea
Antonio Cunha <antonio.frango@gmail.com>
2022-12-08 11:59:32 -0300

    Exercicios pratico 4.3 - Inverter a palavra

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// Exercicio prático - fatorial de 10
+// Exercício prático - fatorial de 10
 
 let fatorial = 10
 let resultado = fatorial
@@ -7,5 +7,19 @@ for (let index = 1; index < fatorial; index +=1) {
   resultado *= index
 }
 console.log ('O fatorial de ' + fatorial + ' é: ' + resultado)
+
+
+// Exercício prático - inverter a palavra
+
+let word = 'tryber';
+let newWord = ''
+
+for (let index = word.length -1; index >=0; index -=1) {
+  newWord += word[index]
+}
+
+console.log (newWord)
+
+
 
 
