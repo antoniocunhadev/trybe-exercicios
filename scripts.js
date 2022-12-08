b48a171f870cd9d2fc8611f000503597a46f2e6b
Antonio Cunha <antonio.frango@gmail.com>
2022-12-08 10:28:03 -0300

    Exercicios pratico 4.3 - Alterando os valores da soma

--- scripts.js
+++ scripts.js
@@ -10,3 +10,31 @@ if (somaFruits > 15) {
 } else {
   console.log ('Valor menor que 16')
 }
+
+const n = 9 ;
+
+let resultado = 100;
+for (let index = 0; index <= n; index += 1) {
+  resultado -= index;
+};
+console.log(resultado);
+
+const n = 5;
+
+let resultado = 0;
+for (let index = 0; index <= n; index += 1) {
+  resultado =+ index
+};
+console.log(resultado)
+
+let array = []
+
+for (let index = 1; index <= 100; index += 1) {
+  array.push(index)
+}
+console.log (array)
+let soma = 0;
+for (let index = 0; index < array.length; index += 1){
+  soma += array[index];
+}
+console.log(soma)
