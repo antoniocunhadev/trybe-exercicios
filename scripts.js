cf66a2ca65e5a7321f25480025afa781a167b538
Antonio Cunha <antonio.frango@gmail.com>
2022-12-15 20:57:27 -0300

    Exercicios/fundamentos-4.4 - Acessando as chaves e valores

--- scripts.js
+++ scripts.js
@@ -19,8 +19,23 @@ console.log (info[0].origem + ' e ' + info[1].origem)
 console.log (info[0].nota + ' e ' + info[1].origem)
 
 if (info[0].recorrente === 'Sim' && info[1].recorrente === 'Sim') {
-  console.log ('Ambos recorrentes')
+  console.log ('Ambos s')
 }
+
+// Leitura de Objetos
+let leitor = {
+  nome: 'Julia',
+  sobrenome: 'Pessoa',
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: 'O Pior Dia de Todos',
+      autor: 'Daniela Kopsch',
+      editora: 'Tordesilhas',
+    },
+  ],
+};
+console.log ('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo)
 
 
 
