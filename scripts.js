b199661f88c918846db2ea821394b4d646684c44
Antonio Cunha <antonio.frango@gmail.com>
2022-12-21 15:45:03 -0300

    Exercicios/fundamentos-4.4 - exercicios funcoes - retornar um array de inteiros e retornar o numero que mais se repete

--- scripts.js
+++ scripts.js
@@ -100,9 +100,9 @@ function maiorCaracter (string) {
     }
     return maiorPalavra
 }
-console.log (maiorCaracter (['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana', 'parelelogramo']))
+console.log (maiorCaracter (['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']))
 
-// Exemplo sem função 
+// Exemplo sem função
 let string = ['mesa' , 'José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana', 'parelelogramo']
 let stringTotal = string[0]
 
@@ -113,5 +113,28 @@ for (let indice in string) {
 }
 console.log(stringTotal)
 
+//Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
 
- 
+function maisRepetido(numeros) {
+  let contRepetido = 0;
+  let contNumero = 0;
+  let indexNumeroRepetido = 0;
+
+  for (let index in numeros) {
+    let verificaNumero = numeros[index];
+    for (let index2 in numeros) {
+      if (verificaNumero === numeros[index2]) {
+        contNumero += 1;
+      }
+    }
+    if (contNumero > contRepetido) {
+      contRepetido = contNumero;
+      indexNumeroRepetido = index;
+    }
+    contNumero = 0;
+  }
+
+  return numeros[indexNumeroRepetido];
+}
+
+console.log(maisRepetido([2, 3, 2, 5, 8, 2, 3])); // 2
