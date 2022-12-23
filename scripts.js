4aa32bdb45dd8f6eadd1d5095f46bb7370882bab
Antonio Cunha <antonio.frango@gmail.com>
2022-12-23 15:43:54 -0300

    Exercicios pratico 4.5 - alterando a funcao para arrow function parte III

--- scripts.js
+++ scripts.js
@@ -45,3 +45,7 @@ console.log(nome);
 
 const nomeCompleto = (nome, sobrenome) => `${nome} ${sobrenome}`
 console.log(nomeCompleto('Ivan', 'Pires'));
+
+let speed = 121;
+const speedCar = (speed) => speed >=120 ? `Você ultrapassou o limite de velocidade` : `Você está na velocidade permitida`;
+console.log(speedCar(speed));
