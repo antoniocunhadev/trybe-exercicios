60f2beb528da89b5c8d79296e7aef3218e8c552f
Antonio Cunha <antonio.frango@gmail.com>
2022-12-23 13:59:48 -0300

    Exercicios pratico 4.5 - alterando a concatenacao para template literals

--- scripts.js
+++ scripts.js
@@ -4,3 +4,31 @@ function imprimeIdade() {
   }
 }
 imprimeIdade()
+
+  // Executando esse código iremos receber um erro `TypeError: Assignment to constant variable.`
+  const pessoa = {
+    nome: 'Henri',
+    idade: 20
+  }
+ 
+   pessoa = {nome: 'Luna',
+   idade: 19
+  }
+   // Altere essa estrutura para corrigir o erro.
+  console.log('Nome:', pessoa.nome);
+  console.log('Idade:', pessoa.idade);
+
+  const favoriteFood = 'Lasanha';
+  favoriteFood = 'Hamburguer';
+  console.log(favoriteFood);
+
+  const name = 'Adriana';
+  const lastName = 'Soares';
+  console.log(`Olá, ${name} ${lastName}!`);
+  function soma(a,b) {
+    let resultado = a + b;
+    return resultado;
+  }
+  let a = 3;
+  let b = 5;
+  console.log(`O resultado da soma de ${a} e ${b} é: ${a + b})`);
