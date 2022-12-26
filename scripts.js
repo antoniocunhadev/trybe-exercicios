a45d1a5e35e1af858767d375f20eb3d59ebe2f39
Antonio Cunha <antonio.frango@gmail.com>
2022-12-26 12:24:42 -0300

    Exercicios pratico parte II 4.5 - Calculando a area de um circulo utilizando funcao

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Crie uma função que ligue e desligue um motor de um carro.
 
 let statusCar = 'desligado'
 const ligarDesligar = () => statusCar === 'desligado' ? statusCar = 'ligado' : statusCar = 'desligado';
@@ -6,9 +7,6 @@ console.log(`O motor está ${ligarDesligar()}`);
 console.log(`O motor está ${ligarDesligar()}`);
 console.log(`O motor está ${ligarDesligar()}`);
 console.log(`O motor está ${ligarDesligar()}`);
-
-
-
 
 let carDesligado = `desligado`
 function ligarDesligar () {
@@ -25,3 +23,12 @@ console.log(ligarDesligar());
 console.log(ligarDesligar()); 
 console.log(ligarDesligar());
 
+//Crie uma função que calcule a área de um círculo.
+
+let pi = 3.14 // Ou utilizar Math.PI
+circleArea = raio => pi * (raio**2);
+console.log(`Essa é a área do círculo: ${circleArea(5)}`); 
+
+//Area do circulo sem função
+let raio = 5
+console.log (Math.PI * (raio**2))
