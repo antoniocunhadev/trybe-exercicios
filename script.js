b241fef535fc19ede7b21c75d701b71f1297b06f
Antonio Cunha <antonio.frango@gmail.com>
2022-12-31 16:54:50 -0300

    exercicios/fundamentos/5.1 - aplicando conhecimento Dom

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("header-container").style.backgroundColor = "green";
+
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor =
+  "orange";
+
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor =
+  "yellow";
+
+document.getElementById("footer-container").style.backgroundColor = "green";
+
+document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor =
+  "blue";
+
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor =
+  "blue";
+
+  document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor =
+  "brown";
+
+  document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor =
+  "brown";
