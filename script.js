361f5330a518d5e5b1c68ad1101c5948676fb143
Antonio Cunha <antonio.frango@gmail.com>
2023-01-06 11:46:00 -0300

    exercicios/fundamentos/5.3 - Iniciando exercicios

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
-// let ondeTo = document.getElementById('elementoOndeVoceEsta');
-// ondeTo.removeChild(ondeTo.firstElementChild)
-// console.log(ondeTo);
+const firstLi = document.getElementById('first-li');
+const secondLi = document.getElementById('second-li');
+const thirdLi = document.getElementById('third-li');
+const input = document.getElementById('input');
+const myWebpage = document.getElementById('my-spotrybefy');
 
-// utilizando 'for' quando existe mais de um filho
 
-let ondeTo = document.getElementById("elementoOndeVoceEsta");
+// - Copie esse arquivo e edite apenas ele;
+//  - Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 
-let filhos = ondeTo.childNodes;
+// - Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
+//  - Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
 
-for (let index = filhos.length - 1; index >= 0; index -= 1) {
-  const filhoAtual = filhos[index];
+// - Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+// com a classe 'tech';
 
-  if (filhoAtual.id === "elementoOndeVoceEsta") {
-    filhoAtual.removeChild(filhoAtual.firstElementChild);
-  }
+// - Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+// redirecione para alguma página;
+//  - Que tal redirecionar para seu portfólio?
+
+// - Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo;
+
+// Segue abaixo um exemplo do uso de event.target:
+
+
+const resetText = (event) => {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
 }
-console.log(filhoAtual)
+
+firstLi.addEventListener('dblclick', resetText);
+
+// Não precisa passar o parâmetro dentro da callback resetText. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'firstLi'.
