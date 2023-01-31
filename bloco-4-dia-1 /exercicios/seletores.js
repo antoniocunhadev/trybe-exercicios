29de9ccac994e126f60b2e4d580c1695b2ab8b09
Antonio Cunha <antonio.frango@gmail.com>
2023-01-31 16:05:32 -0300

    Manipulando as cores utilizando DOM, apenas no javascript

---
+++ bloco-4-dia-1 /exercicios/seletores.js
@@ -0,0 +1,51 @@
+const header = document.getElementById("header-container");
+header.style.backgroundColor = "green";
+
+const emergencyTasks = document.getElementsByClassName("emergency-tasks")[0];
+emergencyTasks.style.background = "green";
+
+const emergencyTasksHeader = document.querySelectorAll(".emergency-tasks h3");
+for (let index = 0; index < emergencyTasksHeader.length; index += 1) {
+  emergencyTasksHeader[index].style.backgroundColor = "blue";
+}
+
+const noEmergencyTasks =
+  document.getElementsByClassName("no-emergency-tasks")[0];
+noEmergencyTasks.style.backgroundColor = "purple";
+
+const noEmergencyTasksHeader = document.querySelectorAll(
+  ".no-emergency-tasks h3"
+);
+for (let index = 0; index < noEmergencyTasksHeader.length; index += 1) {
+  noEmergencyTasksHeader[index].style.backgroundColor = "black";
+}
+
+const footer = document.querySelector ('#footer-container');
+footer.style.backgroundColor = 'green';
+
+
+
+// const body = document.getElementById("container")
+// body.style.backgroundColor = "green";
+
+// const header = document.getElementById('header-container');
+// header.style.backgroundColor = 'green';
+
+// const emergencyTasks = document.getElementsByClassName('emergency-tasks')[0];
+// emergencyTasks.style.backgroundColor = 'blue';
+
+// const emergencyTasksHeaders = document.querySelectorAll('.emergency-tasks h3');
+// for (let index = 0; index < emergencyTasksHeaders.length; index += 1) {
+//   emergencyTasksHeaders[index].style.backgroundColor = 'purple';
+// }
+
+// const noEmergencyTasks = document.querySelector('.no-emergency-tasks');
+// noEmergencyTasks.style.backgroundColor = 'yellow';
+
+// const noEmergencyTasksHeaders = document.querySelectorAll('.no-emergency-tasks h3');
+// for (let index = 0; index < noEmergencyTasksHeaders.length; index += 1) {
+//   noEmergencyTasksHeaders[index].style.backgroundColor = 'red';
+// }
+
+// const footer = document.getElementById('footer-container');
+// footer.style.backgroundColor = 'green';
