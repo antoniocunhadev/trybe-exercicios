137a6c04299291810bbe877227b1996199a9da9c
Antonio Cunha <antonio.frango@gmail.com>
2023-02-01 16:04:36 -0300

    criando um elemento filho

---
+++ bloco-4-dia-2/criando-elementos.js
@@ -0,0 +1,10 @@
+let pai = document.getElementById("pai");
+let maisUmIrmao = document.createElement("section");
+maisUmIrmao.id = 'maisUmIrmao'
+maisUmIrmao.innerText = 'Mais um irmão'
+
+pai.appendChild(maisUmIrmao);
+
+
+
+
