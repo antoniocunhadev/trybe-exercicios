a90de01efcb204f94d9dc7ce4bbb98b0809b3f5d
Antonio Cunha <antonio.frango@gmail.com>
2023-02-03 19:13:07 -0300

    Aplicando WebStorage

--- bloco-4/bloco-4-dia-4/script.js
+++ bloco-4/bloco-4-dia-4/script.js
@@ -1,19 +1,38 @@
-const myObj = {
-  name: 'Link',
-  age: 20,
-};
+const button = document.getElementById('add-button');
+const input = document.getElementById('phrases-input');
+const list = document.getElementById('phrases-list');
 
-// Precisamos transformar este objeto em uma string. Para isso, utilizamos a função `JSON.stringify()`.
+const insertPhraseInDOM = () => {
+  const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+  const listLength = phrasesList.length - 1;
+  const phraseText = phrasesList[listLength];
+  const phrase = document.createElement('li');
+  phrase.innerText = phraseText;
+  list.appendChild(phrase);
+}
 
-localStorage.setItem('myData', JSON.stringify(myObj));
+const addPhraseToLocalStorage = () => {
+  const oldList = JSON.parse(localStorage.getItem('phrases'));
+  const phraseText = input.value;
+  oldList.push(phraseText);
+  localStorage.setItem('phrases', JSON.stringify(oldList));
+  insertPhraseInDOM();
+}
 
-// Como dito anteriormente, o `localStorage` armazena dados no formato chave e valor, sendo os valores sempre string. Para isso, precisamos passar como primeiro parâmetro do método `setItem()` o nome da chave, passamos `myData` e como segundo parâmetro, a função `JSON.stringify()` com o objeto-valor que queremos armazenar na chave `myData`. Feito isso, teremos salvo no `localStorage` um objeto com a chave `myData` e o valor `{ "name": "Link", "age": 20 }`.
+const initialRendering = () => {
+  if (localStorage.getItem('phrases') === null) {
+    localStorage.setItem('phrases', JSON.stringify([]));
+  } else {
+    const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+    const listLength = phrasesList.length - 1;
+    for (let index = 0; index <= listLength; index += 1) {
+      const listElement = document.createElement('li');
+      listElement.innerText = phrasesList[index];
+      list.appendChild(listElement);
+    }
+  }
+}
 
+button.addEventListener('click', addPhraseToLocalStorage);
 
-// Agora, precisamos recuperar o objeto armazenado, mas queremos ele no formato original e não como uma string. Para isso, utilizaremos a função `JSON.parse()`.
-
-const recoveredObject = JSON.parse(localStorage.getItem('myData'));
-
-// Utilizamos a função `JSON.parse()` para transformar a string que está armazenada no `localStorage` de volta para seu formato original, um objeto.
-
-console.log(recoveredObject); // { name: 'Link', age: 20 }
+window.onload = () => initialRendering();
