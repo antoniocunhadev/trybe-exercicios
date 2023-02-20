06a3259710172cabfedd6b6a17e5c3d0e1f7887b
Antonio Cunha <antonio.frango@gmail.com>
2023-02-20 20:10:53 -0300

    Object entries

---
+++ bloco-6/bloco-6-dia-01/object.entries.js
@@ -0,0 +1,24 @@
+const coutries = {
+  franca: 'Paris',
+  brasil: 'Brasília',
+  espanha: 'Madrid',
+  portugal: 'Lisboa',
+};
+
+
+const pairKeyValue = Object.entries(coutries);
+console.log(pairKeyValue);
+
+// for(index in pairKeyValue) {
+//   console.log('--------');
+//   console.log('País:', pairKeyValue[index][0]);
+//   console.log('Capital:', pairKeyValue[index][1]);
+// };
+
+for (index in pairKeyValue){
+  console.log('--------');
+  console.log(pairKeyValue[index][0]);
+  console.log(pairKeyValue[index][1]);
+  
+}
+
