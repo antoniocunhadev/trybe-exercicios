1d55ea57caa6463344d8be3aeeb3df214e725104
Antonio Cunha <antonio.frango@gmail.com>
2023-03-02 20:50:10 -0300

    array.some - praticando

---
+++ bloco-7/bloco7-dia-01/array.some.js
@@ -0,0 +1,19 @@
+const pessoas = [
+  { nome: 'Ana', cargo: 'Analista' },
+  { nome: 'João', cargo: 'Gerência' },
+  { nome: 'Aline', cargo: 'Analista' },
+  { nome: 'Joana', cargo: 'Gerência' },
+];
+
+let gerente = pessoas.some((pessoas) => pessoas.cargo === 'Gerência')
+let proprietario = pessoas.some((pessoas) => pessoas.cargo === 'Product Owner')
+
+console.log(gerente);
+console.log(proprietario);
+
+const listNames = ['Maria', 'Manuela', 'Jorge', 'Ricardo', 'Wilson'];
+
+const verifyFirstLetter = (letter, names) => names.some((name) => name[0] === letter);
+
+console.log(verifyFirstLetter('J', listNames)); // true
+console.log(verifyFirstLetter('X', listNames)); // false
