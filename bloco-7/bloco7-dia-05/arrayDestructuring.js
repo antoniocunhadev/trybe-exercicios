1277d44fa7dd478a83db27c7a9c74b5519e6ec0d
Antonio Cunha <antonio.frango@gmail.com>
2023-03-08 11:36:01 -0300

    Iniciando bloco 07 dia 05

---
+++ bloco-7/bloco7-dia-05/arrayDestructuring.js
@@ -0,0 +1,10 @@
+const user = {
+  name: 'Marlete',
+  age: 60
+};
+
+// desestruturamos a propriedade name e atribuímos como variável
+const { name } = user;
+
+// conseguimos utilizar a propriedade como variável
+console.log(name); // Marlete
